92cb9a1fd344af0385bbfe65202d22c08bb0140f
ericstober <erictstober@gmail.com>
2024-09-24 08:47:37 -0500

    add stripe purchase success page and updates to checkout form

--- src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { userOrderExists } from "@/app/actions/orders";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -10,13 +11,20 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { formatCurrency } from "@/lib/formatters";
-import { Elements, PaymentElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import {
+  Elements,
+  LinkAuthenticationElement,
+  PaymentElement,
+  useElements,
+  useStripe,
+} from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import Image from "next/image";
 import { FormEvent, useState } from "react";
 
 type CheckoutFormProps = {
   product: {
+    id: string;
     imagePath: string;
     name: string;
     priceInCents: number;
@@ -29,48 +37,65 @@ const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY as st
 
 export function CheckoutForm({ product, clientSecret }: CheckoutFormProps) {
   return (
-    <>
-      <div className='max-w-5xl w-full mx-auto space-y-8'>
-        <div className='flex gap-4 items-center'>
-          <div className='aspect-video flex-shrink-0 w-1/3 relative'>
-            <Image src={product.imagePath} fill alt={product.name} className='object-cover' />
-          </div>
-
-          <div>
-            <div className='text-lg'>{formatCurrency(product.priceInCents / 100)}</div>
-            <h1 className='text-2xl font-bold'>{product.name}</h1>
-            <div className='line=clamp-3 text-muted-foreground'>{product.description}</div>
-          </div>
+    <div className='max-w-5xl w-full mx-auto space-y-8'>
+      <div className='flex gap-4 items-center'>
+        <div className='aspect-video flex-shrink-0 w-1/3 relative'>
+          <Image src={product.imagePath} fill alt={product.name} className='object-cover' />
         </div>
 
-        <Elements options={{ clientSecret }} stripe={stripePromise}>
-          <Form priceInCents={product.priceInCents} />
-        </Elements>
+        <div>
+          <div className='text-lg'>{formatCurrency(product.priceInCents / 100)}</div>
+          <h1 className='text-2xl font-bold'>{product.name}</h1>
+          <div className='line=clamp-3 text-muted-foreground'>{product.description}</div>
+        </div>
       </div>
-    </>
+
+      <Elements options={{ clientSecret }} stripe={stripePromise}>
+        <Form priceInCents={product.priceInCents} productId={product.id} />
+      </Elements>
+    </div>
   );
 }
 
-function Form({ priceInCents }: { priceInCents: number }) {
+function Form({ priceInCents, productId }: { priceInCents: number; productId: string }) {
   const stripe = useStripe();
   const elements = useElements();
   const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState<string>();
+  const [email, setEmail] = useState<string>();
 
-  function handleSubmit(e: FormEvent) {
+  async function handleSubmit(e: FormEvent) {
     e.preventDefault();
 
-    if (stripe == null || elements == null) return;
+    if (stripe == null || elements == null || email == null) return;
 
     setIsLoading(true);
 
-    // Check for existing order
+    const orderExists = await userOrderExists(email, productId);
 
-    stripe.confirmPayment({
-      elements,
-      confirmParams: {
-        return_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/stripe/purchase-success`,
-      },
-    });
+    if (orderExists) {
+      setErrorMessage(
+        "You have already purchased this product. Try downloading it from the My Orders page."
+      );
+      setIsLoading(false);
+      return;
+    }
+
+    stripe
+      .confirmPayment({
+        elements,
+        confirmParams: {
+          return_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/stripe/purchase-success`,
+        },
+      })
+      .then(({ error }) => {
+        if (error.type === "card_error" || error.type === "validation_error") {
+          setErrorMessage(error.message);
+        } else {
+          setErrorMessage("An unknown error occured");
+        }
+      })
+      .finally(() => setIsLoading(false));
   }
 
   return (
@@ -78,11 +103,16 @@ function Form({ priceInCents }: { priceInCents: number }) {
       <Card>
         <CardHeader>
           <CardTitle>Checkout</CardTitle>
-          <CardDescription className='text-destructive'>Error</CardDescription>
+          {errorMessage && (
+            <CardDescription className='text-destructive'>{errorMessage}</CardDescription>
+          )}
         </CardHeader>
 
         <CardContent>
           <PaymentElement />
+          <div className='mt-4'>
+            <LinkAuthenticationElement onChange={(e) => setEmail(e.value.email)} />
+          </div>
         </CardContent>
 
         <CardFooter>
